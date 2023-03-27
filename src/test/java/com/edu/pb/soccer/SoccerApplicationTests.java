214c5c599afe2505ebea6152e75ec28bd8c939ef
mkrupinski <maciej.krupinski@xentivo.pl>
2023-03-27 17:52:07 +0200

    init

---
+++ src/test/java/com/edu/pb/soccer/SoccerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.pb.soccer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
